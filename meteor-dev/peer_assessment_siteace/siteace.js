ecd5a1c644660a63bb8755c6628c4abeb312ece6
Sargon Hasso <Sargon Hasso>
2016-08-05 23:42:42 -0500

    basline before adding routing

---
+++ meteor-dev/peer_assessment_siteace/siteace.js
@@ -0,0 +1,130 @@
+Websites = new Mongo.Collection("websites");
+
+if (Meteor.isClient) {
+
+	/////
+	// template helpers 
+	/////
+
+    /// accounts config
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_AND_EMAIL"
+    });
+
+	// helper function that returns all available websites
+	Template.website_list.helpers({
+		websites:function(){
+		    return Websites.find({}, { sort: { voting: -1 } });
+		}
+	});
+
+
+	/////
+	// template events 
+	/////
+
+	Template.website_item.events({
+		"click .js-upvote":function(event){
+			// example of how you can access the id for the website in the database
+			// (this is the data context for the template)
+			var website_id = this._id;
+			console.log("Up voting website with id "+website_id);
+		    // put the code in here to add a vote to a website!
+			var current_web = Websites.findOne({ _id: website_id });
+			if (current_web.voting == undefined) {
+			    current_web.voting = 0;
+			}
+			var vote = current_web.voting + 1;
+            console.log("voting: " + current_web.voting)
+		    Websites.update({ _id: website_id },
+                     { $set: {voting: vote } }
+           );
+			return false;// prevent the button from reloading the page
+		}, 
+		"click .js-downvote":function(event){
+
+			// example of how you can access the id for the website in the database
+			// (this is the data context for the template)
+			var website_id = this._id;
+			console.log("Down voting website with id "+website_id);
+
+			// put the code in here to remove a vote from a website!
+			var current_web = Websites.findOne({ _id: website_id });
+			if (current_web.voting == undefined) {
+			    current_web.voting = 0;
+			}
+			var vote = current_web.voting - 1;
+			console.log("voting: " + current_web.voting)
+			Websites.update({ _id: website_id },
+                     { $set: { voting: vote } }
+           );
+			return false;// prevent the button from reloading the page
+		}
+	})
+
+	Template.website_form.events({
+	    "click .js-toggle-website-form":function(event){
+	        $("#website_form").toggle('slow');
+	    }, 
+	    "submit .js-save-website-form":function(event){
+
+	        // here is an example of how to get the url out of the form:
+	        var url = event.target.url.value;
+	        var title = event.target.title.value;
+	        var description = event.target.description.value;
+	        console.log("The url they entered is: " + url);
+	        console.log("title: " + title);
+	        console.log("description: " + description);
+	        
+		    //  put your website saving code in here!	
+			if (Meteor.user()) {
+			    Websites.insert({
+			        url: url,
+			        title: title,
+			        description: description,
+			        createdOn: new Date(),
+			        createdBy: Meteor.user()._id
+                    
+			    })
+			}
+
+			return false;// stop the form submit from reloading the page
+
+		}
+	});
+}
+
+
+if (Meteor.isServer) {
+	// start up function that creates entries in the Websites databases.
+  Meteor.startup(function () {
+    // code to run on server at startup
+    if (!Websites.findOne()){
+    	console.log("No websites yet. Creating starter data.");
+    	  Websites.insert({
+    		title:"Goldsmiths Computing Department", 
+    		url:"http://www.gold.ac.uk/computing/", 
+    		description:"This is where this course was developed.", 
+    		createdOn:new Date()
+    	});
+    	 Websites.insert({
+    		title:"University of London", 
+    		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route", 
+    		description:"University of London International Programme.", 
+    		createdOn:new Date()
+    	});
+    	 Websites.insert({
+    		title:"Coursera", 
+    		url:"http://www.coursera.org", 
+    		description:"Universal access to the world’s best education.", 
+    		createdOn:new Date()
+    	});
+    	Websites.insert({
+    		title:"Google", 
+    		url:"http://www.google.com", 
+    		description:"Popular search engine.", 
+    		createdOn:new Date()
+    	});
+    }
+  });
+}
