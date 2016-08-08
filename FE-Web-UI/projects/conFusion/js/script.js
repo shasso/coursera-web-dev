7e4d60e81f76474031a9478bff53704689372844
Sargon Hasso <Sargon Hasso>
2016-08-08 13:42:52 -0500

    adding jquery code to control carousel behavior

--- FE-Web-UI/projects/conFusion/js/script.js
+++ FE-Web-UI/projects/conFusion/js/script.js
@@ -3,3 +3,14 @@
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carousel-pause").click(function () {
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function () {
+        $("#mycarousel").carousel('cycle');
+    });
+});
+
