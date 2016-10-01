0ff003f0df758b5c20b20a96086444756ba898d6
Sargon Hasso <Sargon Hasso>
2016-10-01 11:22:37 -0500

    fix code to adhere to requirements

--- angular/module2-solution/app.js
+++ angular/module2-solution/app.js
@@ -1,66 +1,99 @@
 (function () {
     'use strict';
 
-    var shoppingList = [
-        {
-            name: "Milk",
-            quantity: "2"
-        },
-        {
-            name: "Donuts",
-            quantity: "200"
-        },
-        {
-            name: "Cookies",
-            quantity: "300"
-        },
-        {
-            name: "Chocolate",
-            quantity: "5"
-        },
-        {
-            name: "yogurt",
-            quantity: 2
-        }
-    ];
-
-    var alreadyBoughtList = [];
-    var totalItems2Buy = shoppingList.length;
-    var totalItemsBought = 0;
 
     angular.module('ShoppingListApp', [])
-        .controller('ShoppingListController', ShoppingListController);
+        .controller('ToBuyShoppingController', ToBuyShoppingController)
+        .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-    ShoppingListController.$inject = ['$scope'];
+    ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyShoppingController(ShoppingListCheckOffService) {
+        var items2Buy = this;     // you use items2Buy in 'controller as items2Buy' in index.html
 
-    function ShoppingListController($scope) {
-        $scope.shoppingList = shoppingList;
-        $scope.alreadyBoughtList = alreadyBoughtList;
-        $scope.totalItems2Buy = totalItems2Buy;
-        $scope.totalItemsBought = totalItemsBought;
-        $scope.boughtMsg = '';
-        $scope.toBuyMsg = 'Nothing bought yet.';
+        // return items to display
+        items2Buy.items = ShoppingListCheckOffService.getShoppingList();
 
-        $scope.checkoff = function () {
-            console.log('clicked on item: ' + this.$index);
 
-            // add element to alreadyBoughtList
-            $scope.alreadyBoughtList.push(shoppingList[this.$index]);
-            if (shoppingList.length > 0 ) {
-                $scope.toBuyMsg = '';
+        items2Buy.checkoff = function (itemIndex) {
+            //console.log('removing:' + items2Buy.items[itemIndex].name, + ' ' + items2Buy.items[itemIndex].quantity);
+            ShoppingListCheckOffService.checkOff(itemIndex);
+        }
+
+        items2Buy.isEmpty = function () {
+            var items = ShoppingListCheckOffService.getShoppingList();
+            // console.log('length: ' + items.length)
+            return items.length == 0 ? true : false;
+        }
+    }
+
+    AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+        var itemsBought = this;
+
+        // return items to display
+        itemsBought.items = ShoppingListCheckOffService.getAleadyBought();
+
+        itemsBought.isEmpty = function () {
+            var items = ShoppingListCheckOffService.getAleadyBought();
+            // console.log('length: ' + items.length)
+            return items.length == 0 ? true : false;
+        }
+    }
+
+    // service as function object
+    function ShoppingListCheckOffService() {
+        var service = this;
+
+        // toBuy list
+        var toBuy = [
+            {
+                name: "Milk",
+                quantity: "2"
+            },
+            {
+                name: "Donuts",
+                quantity: "200"
+            },
+            {
+                name: "Cookies",
+                quantity: "300"
+            },
+            {
+                name: "Chocolate",
+                quantity: "5"
+            },
+            {
+                name: "yogurt",
+                quantity: 2
             }
+        ];
 
-            // now remove elememnt from shoppingList  
-            // use splice trick method of an array to remove item 
-            // from list; using filter is another way 
-            $scope.shoppingList.splice(this.$index, 1);
-            console.log( $scope.shoppingList.length);
-            if ( $scope.shoppingList.length == 0 ) {                
-                $scope.boughtMsg = 'Everything is bought!'
-            }
+        // already bought, initially empty
+        var bought = [];
 
+        // expose these properties to outside world
+        service.getShoppingList = function () {
+            return toBuy;
+        }
+        service.getAleadyBought = function () {
+            return bought;
+        }
 
+        // add items to bough list
+        service.addItem = function (itemName, howMany) {
+            service.getAleadyBought().push({ name: itemName, quantity: howMany });
+        }
 
+        // remove items from toBuy list
+        service.removeItem = function (itemIndex) {
+            toBuy.splice(itemIndex, 1);
+        }
+        // check off: removes item from toBuy list and add it to bought list
+        service.checkOff = function (itemIndex) {
+            // console.log(itemIndex);
+            service.addItem(toBuy[itemIndex].name, toBuy[itemIndex].quantity);
+            service.removeItem(itemIndex)
         }
 
     }
